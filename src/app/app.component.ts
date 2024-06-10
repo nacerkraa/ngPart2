63f58ba44b4c76b75224cc9742198070d02ff794
nacerkraa <nacer.kraa@univ-constantine2.dz>
2024-06-10 15:40:11 -0800

    9.TemplateDrivenForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+
+interface User {
+  name: string,
+  email: string
+}
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'ngPart2';
+  user: any = {
+    name: "",
+    email: ""
+  }
+
+  submitForm(form: NgForm){
+    if (form.valid) {
+      console.log(form.value, this.user)
+    }
+  }
+
+  validateEmail(): boolean{
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+    return emailRegex.test(this.user.email);
+  }
 }

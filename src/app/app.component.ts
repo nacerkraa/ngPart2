37d1e0560b89eeb04f7a1912f3d9dd612e352fe0
nacerkraa <nacer.kraa@univ-constantine2.dz>
2024-06-11 15:24:37 -0800

    9.Components, Components Comunication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgFor } from '@angular/common';
+import { TaskComponent } from './componets/task/task.component';
 
 interface User {
   name: string,
@@ -12,50 +13,15 @@ interface User {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor, ReactiveFormsModule],
+  imports: [RouterOutlet, NgFor, ReactiveFormsModule, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
-  userForm!: FormGroup;
+    tasks: string[] = ["Brech my teeth", "Playing Football", "Giong to the mosque"];
 
-  constructor(private formBuilder: FormBuilder){
-    this.userForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)]],
-      adress: this.formBuilder.group({
-        street: ['', Validators.required],
-        city: ['', Validators.required]
-      }),
-      phoneNumber: this.formBuilder.array([
-        this.formBuilder.control('', [
-          Validators.required,
-          Validators.pattern(/^\d{10}$/)
-        ])
-      ])
-    })
-  }
-
-  get phoneNumber() {
-    return this.userForm.get('phoneNumber') as FormArray;
-  }
-
-  addPhoneNumber() {
-    this.phoneNumber.push(this.formBuilder.control('', [
-      Validators.required,
-      Validators.pattern(/^\d{10}$/)
-    ]));
-  }
-
-  removePhoneNumber(index: number) {
-    this.phoneNumber.removeAt(index);
-  }
-
-  submitForm() {
-    if (this.userForm.valid) {
-        console.log(this.userForm.value)
+    deleteTask(task: string) {
+      this.tasks = this.tasks.filter((t) => t !== task);
     }
-  }
-
 }

d569b7e2fe8e1668bbb42e67d1d25f7cadefd80a
nacerkraa <nacer.kraa@univ-constantine2.dz>
2024-06-10 17:40:38 -0800

    10.ReactivesForms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 interface User {
   name: string,
@@ -11,25 +12,50 @@ interface User {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, NgFor, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
-  user: any = {
-    name: "",
-    email: ""
+  userForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+    this.userForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)]],
+      adress: this.formBuilder.group({
+        street: ['', Validators.required],
+        city: ['', Validators.required]
+      }),
+      phoneNumber: this.formBuilder.array([
+        this.formBuilder.control('', [
+          Validators.required,
+          Validators.pattern(/^\d{10}$/)
+        ])
+      ])
+    })
   }
 
-  submitForm(form: NgForm){
-    if (form.valid) {
-      console.log(form.value, this.user)
+  get phoneNumber() {
+    return this.userForm.get('phoneNumber') as FormArray;
+  }
+
+  addPhoneNumber() {
+    this.phoneNumber.push(this.formBuilder.control('', [
+      Validators.required,
+      Validators.pattern(/^\d{10}$/)
+    ]));
+  }
+
+  removePhoneNumber(index: number) {
+    this.phoneNumber.removeAt(index);
+  }
+
+  submitForm() {
+    if (this.userForm.valid) {
+        console.log(this.userForm.value)
     }
   }
 
-  validateEmail(): boolean{
-    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
-    return emailRegex.test(this.user.email);
-  }
 }
